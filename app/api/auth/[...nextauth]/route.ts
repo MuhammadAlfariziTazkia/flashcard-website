ff49022b197c79bffa4a3e5c046826a83c97569c
Muhammad Alfarizi Tazkia <alfatazkia@gmail.com>
2024-09-30 23:24:09 +0700

    feat: change bcrypt module

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 // app/api/auth/[...nextauth]/route.ts
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
-import { compare } from 'bcryptjs'; // untuk compare password yang di-hash
+import bcrypt from 'bcrypt'; // untuk compare password yang di-hash
 import { getUserByEmail } from '@/lib/db'; // fungsi untuk ambil user dari database
 
 export const authOptions = {
@@ -15,7 +15,7 @@ export const authOptions = {
       async authorize(credentials) {
         const user = await getUserByEmail(credentials?.email);
         
-        if (user && await compare(credentials.password, user.password)) {
+        if (bcrypt.compare(credentials.password, user.password)) {
           return { id: user.id, name: user.name, email: user.email };
         }
         return null;
