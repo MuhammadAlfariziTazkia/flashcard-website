ca3bbc4152ea0e8ce291d37d08b3af5c2b7065e5
Muhammad Alfarizi Tazkia <alfatazkia@gmail.com>
2024-10-04 19:18:16 +0700

    fix: imports

--- components/ui/modal/AddCardModal.tsx
+++ components/ui/modal/AddCardModal.tsx
@@ -39,7 +39,7 @@ export default function AddCardModal({ topicId, closeAction, updateAction, topic
                     type="submit"
                     className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 py-3 w-full bg-black text-white hover:bg-gray-800"
                 >
-                    "Save"
+                    Save
                 </button>
             </form>
             {isLoading && (
--- components/ui/modal/TestModal.tsx
+++ components/ui/modal/TestModal.tsx
@@ -71,7 +71,7 @@ export default function TestModal({ topicId, closeAction, topicName }: Props) {
                             type="submit"
                             className="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 h-10 py-3 w-full bg-black text-white hover:bg-gray-800"
                         >
-                            "Check Answer"
+                            Check Answer
                         </button>
                     </form>
                     {correctness == 2 && (
@@ -84,7 +84,7 @@ export default function TestModal({ topicId, closeAction, topicName }: Props) {
                             <div className="flex justify-end">
                                 <button className="pop-button" onClick={handleNextQuestion}>
                                     <PlayIcon className="button-icon" />
-                                    "Next Card"
+                                    Next Card
                                 </button>
                             </div>
                         </>
